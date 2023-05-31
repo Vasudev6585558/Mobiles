0d8c96c5b33c1b5a4796b5c6dc341a0bc31c0302
Vasudev6585558 <104711064+Vasudev6585558@users.noreply.github.com>
2023-05-31 10:40:13 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -5,3 +5,46 @@ $(document).ready(function() {
     $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
 
 });
+var li_links = document.querySelectorAll(".links ul li");
+
+var view_wraps = document.querySelectorAll(".view_wrap");
+
+var list_view = document.querySelector(".list-view");
+
+var grid_view = document.querySelector(".grid-view");
+
+li_links.forEach(function(link){
+
+	link.addEventListener("click", function(){
+
+		li_links.forEach(function(link){
+
+			link.classList.remove("active");
+
+		})
+
+		link.classList.add("active");
+
+		var li_view = link.getAttribute("data-view");
+
+		view_wraps.forEach(function(view){
+
+			view.style.display = "none";
+
+		})
+
+		if(li_view == "list-view"){
+
+			list_view.style.display = "block";
+
+		}
+
+		else{
+
+			grid_view.style.display = "block";
+
+		}
+
+	})
+
+})
